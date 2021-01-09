88b7b1172c69f339f03eafe2a6ca107e8e472f6e
Aaron Snoswell <aaronsnoswell@gmail.com>
2021-01-09 17:19:17 +1000

    Correctly get port-taxis on path

--- experiments/porto_exp.py
+++ experiments/porto_exp.py
@@ -6,7 +6,7 @@ import random
 import argparse
 import warnings
 
-sys.path.append(os.path.join(__file__, ".."))
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
 import numpy as np
 
