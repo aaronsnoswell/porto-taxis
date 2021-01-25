1b24abed07bd38a76da1935041c585b5a391a5f7
Aaron Snoswell <aaronsnoswell@gmail.com>
2021-01-25 13:39:22 +1000

    Add em_resp_tolerance parameter

--- experiments/porto_taxis_exp.py
+++ experiments/porto_taxis_exp.py
@@ -46,6 +46,9 @@ def base_config():
     # Tolerance for Negative Log Likelihood convergence
     em_nll_tolerance = 0.0
 
+    # Tolerance for Negative Log Likelihood convergence
+    em_resp_tolerance = 1e-3
+
     # How many rollouts to use for training?
     num_train_rollouts = 1000
 
@@ -81,6 +84,7 @@ def porto_taxis(
     num_train_rollouts,
     reward_range,
     em_nll_tolerance,
+    em_resp_tolerance,
     maxent_feature_tolerance,
     num_clusters,
     max_iterations,
@@ -186,32 +190,31 @@ def porto_taxis(
         else:
             raise ValueError()
 
-        def post_em_iteration(solver, iteration, resp, mode_weights, rewards, nll):
+        def post_em_iteration(
+            solver, iteration, resp, mode_weights, rewards, nll, nll_delta, resp_delta
+        ):
             _log.info(f"{_seed}: Iteration {iteration} ended")
             _run.log_scalar("training.nll", nll)
+            _run.log_scalar("training.nll_delta", nll_delta)
+            _run.log_scalar("training.resp_delta", resp_delta)
             for mw_idx, mw in enumerate(mode_weights):
                 _run.log_scalar(f"training.mw{mw_idx}", mw)
             for reward_idx, reward in enumerate(rewards):
                 for theta_idx, theta_val in enumerate(reward.theta):
                     _run.log_scalar(f"training.r{reward_idx}.t{theta_idx}", theta_val)
 
-        if reward_initialisation == "MeanOnly":
-            _log.info(
-                f"{_seed}: Initialisation done - switching to MLE reward model for EM alg"
-            )
-            _log.info(f"{_seed}: Loading MaxEnt solver...")
-            solver = MaxEntEMSolver(
-                # LBFGS convergence threshold (units of km)
-                minimize_kwargs=dict(tol=maxent_feature_tolerance),
-                minimize_options=dict(disp=True, maxfun=max_irl_objective_calls),
-                pre_it=lambda i: _log.info(f"{_seed}: Starting iteration {i}"),
-                post_it=post_em_iteration
-                # parallel_executor=futures.ThreadPoolExecutor(num_clusters),
-            )
-        elif reward_initialisation == "MLE":
-            pass
-        else:
-            raise ValueError
+        _log.info(
+            f"{_seed}: Initialisation done - switching to MLE reward model for EM alg"
+        )
+        _log.info(f"{_seed}: Loading MaxEnt solver...")
+        solver = MaxEntEMSolver(
+            # LBFGS convergence threshold (units of km)
+            minimize_kwargs=dict(tol=maxent_feature_tolerance),
+            minimize_options=dict(disp=True, maxfun=max_irl_objective_calls),
+            pre_it=lambda i: _log.info(f"{_seed}: Starting iteration {i}"),
+            post_it=post_em_iteration
+            # parallel_executor=futures.ThreadPoolExecutor(num_clusters),
+        )
 
         # Evaluate initial mixture model
         _log.info(f"{_seed}: Evaluating initial solution...")
@@ -237,7 +240,8 @@ def porto_taxis(
             reward_range,
             mode_weights=init_mode_weights,
             rewards=init_rewards,
-            tolerance=em_nll_tolerance,
+            nll_tolerance=em_nll_tolerance,
+            resp_tolerance=em_resp_tolerance,
             max_iterations=max_iterations,
         )
         iterations = int(len(resp_history))
@@ -245,13 +249,6 @@ def porto_taxis(
         learned_mode_weights = mode_weights_history[-1]
         learned_rewards = rewards_history[-1]
         nll = float(nll_history[-1])
-
-        _log.info(f"{_seed}: Iterations: {iterations}")
-        _log.info(f"{_seed}: Responsibility Matrix")
-        _log.info(learned_resp)
-        _log.info(f"{_seed}: Mode Weights: {learned_mode_weights}")
-        _log.info(f"{_seed}: Rewards: {[r.theta for r in learned_rewards]}")
-        _log.info(f"{_seed}: Model NLL: {nll}")
 
         # Evaluate trained model
         _log.info(f"{_seed}: Evaluating trained model...")
@@ -263,7 +260,6 @@ def porto_taxis(
             rollouts_test,
             skip_ml_paths,
         )
-
         save_eval_results(
             result_fname,
             init_nlls=init_nlls,
@@ -278,9 +274,13 @@ def porto_taxis(
             learned_resp=learned_resp,
             learned_mode_weights=learned_mode_weights,
             learned_rewards=learned_rewards,
+            resp_history=resp_history,
+            mode_weights_history=mode_weights_history,
+            rewards_history=rewards_history,
             nll=nll,
             reason=reason,
         )
+
         _run.add_artifact(result_fname)
         os.remove(result_fname)
 
@@ -319,7 +319,6 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "-k",
         "--num_modes",
         required=False,
         default=3,
@@ -328,7 +327,6 @@ def main():
     )
 
     parser.add_argument(
-        "-w",
         "--num_workers",
         required=False,
         default=None,
@@ -337,8 +335,7 @@ def main():
     )
 
     parser.add_argument(
-        "-i",
-        "--init",
+        "--initialisation",
         required=False,
         type=str,
         default="Random",
@@ -347,7 +344,7 @@ def main():
     )
 
     parser.add_argument(
-        "--reward_init",
+        "--reward_initialisation",
         required=False,
         default="MLE",
         type=str,
@@ -356,7 +353,14 @@ def main():
     )
 
     parser.add_argument(
-        "-m",
+        "--em_resp_tolerance",
+        required=False,
+        default=1e-3,
+        type=float,
+        help="EM convergence tolerance for the responsibility matrix entries",
+    )
+
+    parser.add_argument(
         "--max_iterations",
         required=False,
         type=int,
@@ -365,7 +369,6 @@ def main():
     )
 
     parser.add_argument(
-        "-N",
         "--num_replicates",
         required=False,
         type=int,
@@ -374,9 +377,9 @@ def main():
     )
 
     parser.add_argument(
-        "--skip_ml_paths",
+        "--eval_ml_paths",
         action="store_true",
-        help="Skip ML path evaluations (speeds up experiment substantially)",
+        help="Perform ML path evaluations (speeds up experiment substantially)",
     )
 
     args = parser.parse_args()
@@ -384,10 +387,11 @@ def main():
 
     _base_config = {
         "num_clusters": args.num_modes,
-        "initialisation": args.init,
-        "reward_initialisation": args.reward_init,
+        "initialisation": args.initialisation,
+        "reward_initialisation": args.reward_initialisation,
+        "em_resp_tolerance": args.em_resp_tolerance,
         "max_iterations": args.max_iterations,
-        "skip_ml_paths": args.skip_ml_paths,
+        "skip_ml_paths": not args.eval_ml_paths,
     }
     print("META: Base configuration: ")
     pprint(_base_config)
